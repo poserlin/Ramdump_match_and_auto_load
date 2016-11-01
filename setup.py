8f72ee56146d464ca5ec6ea764ff30412fa6feef
poser_lin <poser_lini@htc.com>
2016-11-01 14:24:16 +0800

    add the exe generator

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    options={'py2exe': {
+        'bundle_files': 1,
+        'compressed': True,
+    }},
+    console=[{'script': 'ramdumptest-html-parser.py'}],
+    data_files = [("",['config.txt'])],
+    zipfile=None
+)

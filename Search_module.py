f92d2cc12a659c4e1bbf947dbfb7e51955d71581
poser_lin <poser_lini@htc.com>
2017-03-23 13:14:26 -0700

    Speed up the elf copy from SSD server by changing copy buffer size from 16KB to 16MB

--- Search_module.py
+++ Search_module.py
@@ -103,7 +103,10 @@ def search_elf_remote(radio_version):
 
 
         print('>> Found, Copy file from SSD server......', )
-        shutil.copy(elf_file_remote_location, local_elf_file_location)
+        # shutil.copy(elf_file_remote_location, local_elf_file_location)
+        with open(elf_file_remote_location, 'rb') as fin:
+            with open(local_elf_file_location, 'wb') as fout:
+                shutil.copyfileobj(fin, fout, 16*1024*1024)
         shutil.copy(msg_hash_file, local_msg_hash_file_location)
 
         # checking the file size, if match, add _fin in the file name.
